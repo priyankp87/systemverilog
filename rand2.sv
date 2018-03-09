3d445e42ff95d982b0dec4cea5aa5bd97cb07b84
priyankp87 <priyank87@outlook.com>
2018-03-08 20:11:03 -0700

    Update rand2.sv

--- rand2.sv
+++ rand2.sv
@@ -1,29 +1,29 @@
 module rand2();
-  int i;
-  int j;
+    int i;
+    int j;
 
-  class random_dist;
-    rand bit [7:0] i;
-    bit [7:0] j;
-    function new();
-      j = 8;
-    endfunction
-    constraint i_dist {
-      i dist {j:=50};
-    }
-  endclass
+    class random_dist;
+        rand bit [7:0] i;
+        bit [7:0] j;
+        function new();
+            j = 8;
+        endfunction
+        
+        constraint i_dist {
+            i dist {j:=50};
+        }
+    endclass
   
-  // Class handle
-  random_dist random_dist1;
+    // Class handle
+    random_dist random_dist1;
 
-  // Initial block
-  initial begin
-    random_dist1 = new ();
-  
-    for (int i=0; i<100; i++)
-    begin
-      random_dist1.randomize();
-      $display ("Randomized value of i,j is : %d\t%d", random_dist1.i, random_dist1.j);
-    end
-  end // initial-end
+    // Initial block
+    initial begin
+        random_dist1 = new ();
+        for (int i=0; i<100; i++)
+        begin
+            random_dist1.randomize();
+            $display ("Randomized value of i,j is : %d\t%d", random_dist1.i, random_dist1.j);
+        end
+    end // initial-end
 endmodule
